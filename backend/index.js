c31c29586ff61bc510f55d5239c673cfd1909175
Matias Moreno <maatiigmm@gmail.com>
2025-07-24 11:42:39 -0400

    add cors

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 const { sequelize } = require('./models');
 const routes = require('./routes');
+const cors = require('cors');
 
 const PORT = process.env.BACKEND_PORT || 3001;
 
+app.use(cors())
 app.use(express.json());
 app.use('/api', routes);
 
